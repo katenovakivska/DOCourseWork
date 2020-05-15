569ea5ef4f76812e4dea72e8a55f8c0c36b01447
Yuliaaaaaa <jshcherbakova@gmail.com>
2020-05-15 19:40:00 +0300

    Changed the way of working with Files

--- CourseWork/Methods/FileReading.cs
+++ CourseWork/Methods/FileReading.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+﻿//using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using CourseWork.Models;
 
 namespace CourseWork.Methods
 {
@@ -11,27 +12,44 @@ namespace CourseWork.Methods
     {
         public int[,] num;
         public int count = 0;
-        public void FileRead(string file)
+        public Order FileRead(string file)
         {
-            string path = @"C:\Users\Katya\source\repos\CourseWork\CourseWork\Files\";
+            Order order = new Order();
+            order.OrdersProducts = new List<OrderProduct>();
+            string path = @".\Files\";
             string[] lines = File.ReadAllLines($"{path}{file}");
-            num = new int[lines.Length, lines[0].Split(' ').Length];
-            for (int i = 0; i < lines.Length; i++)
+            num = new int[lines[0].Split(';').Length-1, lines.Length-2];
+            for (int i = 1; i < lines.Length; i++)
             {
-                string[] temp = lines[i].Split(' ');
-                for (int j = 0; j < temp.Length; j++)
-                    num[i, j] = Convert.ToInt32(temp[j]);
-                
+                string[] temp = lines[i].Split(';');
+                if(i != lines.Length - 1)
+                {
+                    for (int j = 1; j < temp.Length; j++)
+                        num[j-1,i-1] = Convert.ToInt32(temp[j]);
+                    OrderProduct orderProduct = new OrderProduct();
+                    orderProduct.Product = new Product();
+                    orderProduct.Product.Name = temp[0];
+                    orderProduct.Amount = Convert.ToInt32(temp[1]);
+                    orderProduct.Sum = Convert.ToInt32(temp[2]);
+                    order.OrdersProducts.Add(orderProduct);
+                }
+                else
+                {
+                    order.AllSum = Convert.ToInt32(temp[temp.Length-1]);
+                }
             }
             count = num.GetLength(1);
+            return order;
         }
 
         public List<string> AllFiles()
         {
-            List<string> txtFiles = Directory.GetFiles(@"C:\Users\Katya\source\repos\CourseWork\CourseWork\Files", "*.txt")
+            List<string> csvFiles = Directory.GetFiles(@".\Files\", "*.csv")
                                      .Select(Path.GetFileName)
                                      .ToList();
-            return txtFiles;
+            return csvFiles;
         }
+
+
     }
 }
